0b0689b169cd9d2a46851a960a27faa7a645108c
manashbarai <manashjbarai@gmail.com>
2024-11-08 19:18:48 +0530

    nav styling completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between w-1/2 mx-auto py-2 items-center bg-zinc-700 px-2 rounded-b-2xl sticky top-0 text-zinc-900 shadow shadow-zinc-600 font-semibold'>
+    <div className='flex justify-between w-1/2 mx-auto  items-center bg-zinc-700  rounded-b-2xl sticky top-0 text-zinc-900  font-semibold overflow-hidden'>
 
-        <h1>Home</h1>
+        <h1 className='ml-2 '>Home</h1>
 
-        <ul className='flex gap-3 relative'>
-            <li> <Link  to="" >About</Link> </li>
-            <li> <Link to="#">Project</Link> </li>
-            <li> <Link to="#">Contact</Link> </li>
-            <li> <Link to="#">Resume</Link> </li>
+        <ul className='flex  relative nav'>
+            <li>About </li>
+            <li>Project </li>
+            <li>Contact </li>
+            <li>Resume </li>
         </ul>
       
     </div>
